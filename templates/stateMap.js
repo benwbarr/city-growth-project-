893a46c21b4238556e8a658a3247e9ccdabe0c1f
Jonathan Stafford-Bentley <staffordbentley@gmail.com>
2021-01-23 09:55:41 -0800

    test

---
+++ templates/stateMap.js
@@ -0,0 +1,19 @@
+d3.csv("../AY/SQL/yearly_estimates_state.csv", function(data) {
+    // data => names, metadata, samples //
+    console.log(data)
+    // names : list of id numbers //
+    //console.log(data.names)
+    // metadata => id, ethnicity, gender, age, location //
+    //console.log(data.metadata[0].age)
+    // samples => id, otu_ids, sample_values, out_labels
+    //console.log(data.samples)
+    
+    // inserting id options into dropdown menu
+    //var dropdown = d3.select("#selDataset")
+    //dropdown.selectAll("option")
+        //.data(data.names)
+        //.enter()
+        //.append("option")
+        //.text(entry => {return entry})
+
+    })
